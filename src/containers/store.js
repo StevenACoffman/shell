adb44d8a719c73e57b8e2dfe777bd111e7650864
Mayowa Falade <mayowa.falade@ithaka.org>
2016-10-21 16:29:22 -0400

    narrow down diffs

--- src/containers/store.js
+++ src/containers/store.js
@@ -4,5 +4,4 @@ import reducers from '../reducers/index';
 /* eslint-disable no-underscore-dangle */
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 /* eslint-enable */
-
 export default store;
