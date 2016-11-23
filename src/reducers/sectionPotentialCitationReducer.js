a3c6fe9b3baeac5c0dde9b5c01cc17e9692503bd
Steve Coffman <gears@umich.edu>
2016-11-23 15:45:51 -0500

    Whups. Add the file I missed

---
+++ src/reducers/sectionPotentialCitationReducer.js
@@ -0,0 +1,16 @@
+const sectionPotentialCitationReducer = (state = [], action) => {
+    switch (action.type) {
+    case "CLEAR_SELECTED_LIST_ITEMS":
+        return [];
+    case "SELECT_LIST_ITEM":
+        return [...state, action.listItemIndex];
+    case "UNSELECT_LIST_ITEM":
+        return state.filter((listItemIndex) => (listItemIndex !== action.listItemIndex));
+    case "SELECT_ALL_LIST_ITEM":
+        return action.listItems;
+    default:
+        return state;
+    }
+};
+
+export default sectionPotentialCitationReducer;
