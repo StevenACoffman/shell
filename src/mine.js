aa9daa11e120469e56a334d0a97e05c1ee3fdcfc
Steve Coffman <gears@umich.edu>
2016-11-28 15:09:29 -0500

    Add missing files

---
+++ src/mine.js
@@ -0,0 +1,27 @@
+const mine = [
+    {
+        title: [
+            "Unpacking the Monad: Leibniz's Theory of Causality"
+        ],
+        citation_line: "<cite>The Monist</cite>, Vol. 79, No. 3, Causality Before Hume (JULY 1996), pp. 408-425",
+        author: [
+            "Marc Bobro",
+            "Kenneth Clatterbaugh"
+        ],
+        doi: "10.2307/27903491",
+        mla: "Bobro, Marc, and Kenneth Clatterbaugh. “Unpacking the Monad: Leibniz's Theory of Causality.” <i>The Monist</i>, vol. 79, no. 3, 1996, pp. 408–425. www.jstor.org/stable/27903491.",
+        apa: "Loading Formatted Citation",
+        chicago: "Loading Formatted Citation",
+        isFetching: false
+    },
+    {
+        title: [
+            "Nonstandard Topology and Extensions of Monad Systems to Infinite Points"
+        ],
+        citation_line: "<cite>The Journal of Symbolic Logic</cite>, Vol. 36, No. 3 (Sep., 1971), pp. 463-476",
+        author: [
+            "Frank Wattenberg"
+        ],
+        doi: "10.2307/2269956",
+    }
+];
